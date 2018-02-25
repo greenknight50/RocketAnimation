3d66b06fb44f035296c3ce807cfe8cafc6e735ab
Ashraf Mohammed <ashraf.mohammed@redmorph.com>
2018-02-25 19:42:49 +0530

    First commit

--- RocketAnimation/ViewController.swift
+++ RocketAnimation/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var dekhoLbl: UILabel!
+    @IBOutlet weak var udiLbl: UILabel!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay();
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func powerBtnPressed(_ sender: Any) {
+        
+        darkBlueBG.isHidden = true
+        powerBtn.isHidden = true
+        cloudHolder.isHidden = false
+        player.play()
+        UIView.animate(withDuration: 2.3, animations: {
+            self.rocket.frame = CGRect(x: 0, y: 160, width: 375, height: 305)
+        }) { (finished) in
+            self.dekhoLbl.isHidden = false
+            self.udiLbl.isHidden = false
+        }
     }
-
-
 }
 
